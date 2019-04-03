b3748476ff70b37c9141b7c6b5aa4ab5a61ac261
yinglan <yinglanou19@gmail.com>
2019-04-02 17:21:52 -0700

    add create and update methods

--- ninety_nine_cats/app/controllers/cats_controller.rb
+++ ninety_nine_cats/app/controllers/cats_controller.rb
@@ -15,11 +15,13 @@ class CatsController < ApplicationController
     end
 
     def new
-
+        @cat = Cat.new
+        render :new
     end
     
     def create
-
+        @cat = Cat.create(cat_params)
+        redirect_to cat_url(@cat)
     end
 
     def edit
@@ -29,7 +31,14 @@ class CatsController < ApplicationController
     end
 
     def update
+        @cat = Cat.update(cat_params)
+        redirect_to cat_url(@cat)
+    end
 
+    private
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description )
     end
 
 end
